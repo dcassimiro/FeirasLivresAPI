6252f4b12879ac3855f89a98d13975ba099acca5
dcassimiro <dannilo.cassimiro@acessodigital.com.br>
2021-06-11 17:00:08 -0300

    wip

--- api/v1/v1.go
+++ api/v1/v1.go
@@ -9,10 +9,6 @@ import (
 func Register(g *echo.Group, apps *app.Container) {
 	v1 := g.Group("/v1")
 
-	// health.Register(v1.Group("/health"), apps, middleware)
-	// mock.Register(v1.Group("/mock"), apps, middleware)
-	//feira.Register(v1.Group("/feiras"), apps)
-
 	feira.Register(v1.Group("/feiras"), apps)
 
 }
