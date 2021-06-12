dde9b5d7553dc3b0ce2b573c09a112668ca59d2f
dcassimiro <dannilo.cassimiro@acessodigital.com.br>
2021-06-12 12:49:23 -0300

    wip - model feira

--- model/feira.go
+++ model/feira.go
@@ -1,21 +1,47 @@
 package model
 
+import "time"
+
+type FeiraRequest struct {
+	IDFEIRA     string `json:"idfeira"`
+	XLONG       string `json:"long"`
+	LAT         string `json:"lat"`
+	SETCENS     string `json:"setcens"`
+	AREAP       string `json:"areap"`
+	CODDIST     string `json:"coddist"`
+	DISTRITO    string `json:"distrito"`
+	CODSUBPREF  string `json:"codsubpref"`
+	SUBPREFE    string `json:"subprefe"`
+	REGIAO5T    string `json:"regiao5t"`
+	REGIAO8T    string `json:"regiao8t"`
+	NOMEFEIRA   string `json:"nomefeira"`
+	REGISTROT   string `json:"registrot"`
+	LOGRADOUROT string `json:"logradourot"`
+	NUMEROT     string `json:"numerot"`
+	BAIRROT     string `json:"bairrot"`
+	REFERENCIAT string `json:"referenciat"`
+}
+
 type Feira struct {
-	ID          string `json:"id" db:"id"`
-	LONG        string `json:"" db:""`
-	LAT         string `json:"" db:""`
-	SETCENS     string `json:"" db:""`
-	AREAP       string `json:"" db:""`
-	CODDIST     string `json:"" db:""`
-	DISTRITO    string `json:"distrito" db:"distrito"`
-	CODSUBPREF  string `json:"" db:""`
-	SUBPREFE    string `json:"" db:""`
-	REGIAO5T    string `json:"" db:""`
-	REGIAO8T    string `json:"" db:""`
-	NOMEFEIRA   string `json:"" db:""`
-	REGISTROT   string `json:"" db:""`
-	LOGRADOUROT string `json:"" db:""`
-	NUMEROT     string `json:"" db:""`
-	BAIRROT     string `json:"" db:""`
-	REFERENCIAT string `json:"" db:""`
+	Id          string     `json:"id"`
+	IDFEIRA     string     `json:"idfeira"`
+	XLONG       string     `json:"long"`
+	LAT         string     `json:"lat"`
+	SETCENS     string     `json:"setcens"`
+	AREAP       string     `json:"areap"`
+	CODDIST     string     `json:"coddist"`
+	DISTRITO    string     `json:"distrito"`
+	CODSUBPREF  string     `json:"codsubpref"`
+	SUBPREFE    string     `json:"subprefe"`
+	REGIAO5T    string     `json:"regiao5t"`
+	REGIAO8T    string     `json:"regiao8t"`
+	NOMEFEIRA   string     `json:"nomefeira"`
+	REGISTROT   string     `json:"registrot"`
+	LOGRADOUROT string     `json:"logradourot"`
+	NUMEROT     string     `json:"numerot"`
+	BAIRROT     string     `json:"bairrot"`
+	REFERENCIAT string     `json:"referenciat"`
+	CreatedAt   time.Time  `json:"createdat"`
+	UpdatedAt   *time.Time `json:"updatedat"`
+	DeletedAt   *time.Time `json:"deletedat"`
 }
