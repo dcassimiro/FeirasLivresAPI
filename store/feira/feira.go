46684de6110fa9632493a96a51d749ecb40ca8a6
dcassimiro <dannilo.cassimiro@acessodigital.com.br>
2021-06-13 22:50:04 -0300

    fix store

--- store/feira/feira.go
+++ store/feira/feira.go
@@ -2,10 +2,13 @@ package feira
 
 import (
 	"context"
-	"fmt"
+	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/unico/FeirasLivresAPI/fverr"
+	"github.com/unico/FeirasLivresAPI/logger"
 	"github.com/unico/FeirasLivresAPI/model"
 )
 
@@ -30,17 +33,19 @@ type storeImpl struct {
 func (r *storeImpl) Create(ctx context.Context, feira model.FeiraRequest) (string, error) {
 
 	result, err := r.writer.ExecContext(ctx, `
-		INSERT INTO feira (idfeira, xlong, lat, setcens, areap, coddist, distrito, codsubpref, subprefe, regiao5t, regiao8t, nomefeira, registrot, logradourot, numerot, bairrot, referenciat)
+		INSERT INTO feira (ID_FEIRA, XLONG, LAT, SETCENS, AREAP, CODDIST, DISTRITO, CODSUBPREF, SUBPREFE, REGIAO5, REGIAO8, NOME_FEIRA, REGISTRO, LOGRADOURO, NUMERO, BAIRRO, REFERENCIA)
 		VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-	`, feira.IDFEIRA, feira.XLONG, feira.LAT, feira.SETCENS, feira.AREAP, feira.CODDIST, feira.DISTRITO, feira.CODSUBPREF, feira.SUBPREFE, feira.REGIAO5T, feira.REGIAO8T, feira.NOMEFEIRA, feira.REGISTROT, feira.LOGRADOUROT, feira.NUMEROT, feira.BAIRROT, feira.REFERENCIAT)
+	`, feira.ID_FEIRA, feira.XLONG, feira.LAT, feira.SETCENS, feira.AREAP, feira.CODDIST, feira.DISTRITO, feira.CODSUBPREF, feira.SUBPREFE, feira.REGIAO5, feira.REGIAO8, feira.NOME_FEIRA, feira.REGISTRO, feira.LOGRADOURO, feira.NUMERO, feira.BAIRRO, feira.REFERENCIA)
 	if err != nil {
-		fmt.Println(ctx, "store.feira.Create", err.Error())
-		return "", err
+		logger.ErrorContext(ctx, "store.feira.Create", err.Error())
+		logger.L.Println(time.Now(), "não foi possível criar uma nova feira:", err.Error())
+		return "", fverr.New(http.StatusInternalServerError, "não foi possível criar uma nova feira", nil)
 	}
 
 	id, err := result.LastInsertId()
 	if err != nil {
-		fmt.Println(ctx, "store.feira.Create", err.Error())
+		logger.ErrorContext(ctx, "store.feira.Create", err.Error())
+		logger.L.Println(time.Now(), "store.feira.Create:", err.Error())
 	}
 
 	return strconv.FormatInt(id, 10), nil
@@ -50,33 +55,34 @@ func (r *storeImpl) ReadOne(ctx context.Context, id string) (*model.Feira, error
 	feira := new(model.Feira)
 	err := r.writer.GetContext(ctx, feira, `
 		SELECT
-			id,
-			idfeira,
-			xlong,
-			lat,
-			setcens,
-			areap,
-			coddist,
-			distrito,
-			codsubpref,
-			subprefe,
-			regiao5t,
-			regiao8t,
-			nomefeira,
-			registrot,
-			logradourot,
-			numerot,
-			bairrot,
-			referenciat,
-			createdat,
-			updatedat,
-			deletedat
+			ID,
+			ID_FEIRA,
+			XLONG,
+			LAT,
+			SETCENS,
+			AREAP,
+			CODDIST,
+			DISTRITO,
+			CODSUBPREF,
+			SUBPREFE,
+			REGIAO5,
+			REGIAO8,
+			NOME_FEIRA,
+			REGISTRO,
+			LOGRADOURO,
+			NUMERO,
+			BAIRRO,
+			REFERENCIA,
+			CREATED_AT,
+			UPDATED_AT,
+			DELETED_AT
 		FROM feira 
 		WHERE
-			id = ?;
+			ID = ?;
 	`, id)
 	if err != nil {
-		fmt.Println(ctx, "store.feira.ReadOne", err.Error())
+		logger.ErrorContext(ctx, "store.feira.ReadOne", err.Error())
+		logger.L.Println(time.Now(), "store.feira.ReadOne:", err.Error())
 		return nil, err
 	}
 
@@ -85,40 +91,45 @@ func (r *storeImpl) ReadOne(ctx context.Context, id string) (*model.Feira, error
 
 func (r *storeImpl) Update(ctx context.Context, id string, feira model.FeiraRequest) (string, error) {
 	_, err := r.writer.ExecContext(ctx, `
-		UPDATE feira 
-		SET 
-		xlong = ?,
-		lat = ?,
-		setcens = ?,
-		areap = ?,
-		coddist = ?,
-		distrito = ?,
-		codsubpref = ?,
-		subprefe = ?,
-		regiao5t = ?,
-		regiao8t = ?,
-		nomefeira = ?,
-		registrot = ?,
-		logradourot = ?,
-		numerot = ?,
-		bairrot = ?,
-		referenciat = ? ,
-		updatedat = CURRENT_TIMESTAMP()
-		WHERE id = ? AND deletedat IS NULL;
-	`, feira.XLONG, feira.LAT, feira.SETCENS, feira.AREAP, feira.CODDIST, feira.DISTRITO, feira.CODSUBPREF, feira.SUBPREFE, feira.REGIAO5T, feira.REGIAO8T, feira.NOMEFEIRA, feira.REGISTROT, feira.LOGRADOUROT, feira.NUMEROT, feira.BAIRROT, feira.REFERENCIAT, id)
+		UPDATE feira SET
+		XLONG = ?,
+		LAT = ?,
+		SETCENS = ?,
+		AREAP = ?,
+		CODDIST = ?,
+		DISTRITO = ?,
+		CODSUBPREF = ?,
+		SUBPREFE = ?,
+		REGIAO5 = ?,
+		REGIAO8 = ?,
+		NOME_FEIRA = ?,
+		REGISTRO = ?,
+		LOGRADOURO = ?,
+		NUMERO = ?,
+		BAIRRO = ?,
+		REFERENCIA = ?,
+		UPDATED_AT = CURRENT_TIMESTAMP()
+		WHERE ID = ? AND DELETED_AT IS NULL;
+	`, feira.XLONG, feira.LAT, feira.SETCENS, feira.AREAP, feira.CODDIST, feira.DISTRITO, feira.CODSUBPREF, feira.SUBPREFE, feira.REGIAO5, feira.REGIAO8, feira.NOME_FEIRA, feira.REGISTRO, feira.LOGRADOURO, feira.NUMERO, feira.BAIRRO, feira.REFERENCIA, id)
 	if err != nil {
-		fmt.Println(ctx, "store.company.Update", err.Error())
-		return "", err
+		logger.ErrorContext(ctx, "store.feira.Update", err.Error())
+		logger.L.Println(time.Now(), "não foi possível modificar a feira:", err.Error())
+		return "", fverr.New(http.StatusInternalServerError, "não foi possível modificar a feira", nil)
 	}
 
 	return id, nil
 }
 
 func (r *storeImpl) Delete(ctx context.Context, id string) error {
-	_, err := r.writer.ExecContext(ctx, `UPDATE feira SET deleted_at = CURRENT_TIMESTAMP() WHERE id = ?;`, id)
+	_, err := r.writer.ExecContext(ctx, `UPDATE feira SET DELETED_AT = CURRENT_TIMESTAMP() WHERE id = ?;`, id)
 	if err != nil {
-		return err
+		logger.ErrorContext(ctx, "store.feira.Delete", err.Error())
+		logger.L.Println(time.Now(), "não foi possível deletar a feira:", err.Error())
+		return fverr.New(http.StatusInternalServerError, "não foi possível deletar a feira", map[string]string{
+			"id": id,
+		})
 	}
+
 	return nil
 }
 
@@ -126,34 +137,38 @@ func (r *storeImpl) Search(ctx context.Context, distrito string) ([]*model.Feira
 	var feiras []*model.Feira
 	err := r.reader.SelectContext(ctx, &feiras, `
 	SELECT 
-		id,
-		idfeira,
-		xlong,
-		lat,
-		setcens,
-		areap,
-		coddist,
-		distrito,
-		codsubpref,
-		subprefe,
-		regiao5t,
-		regiao8t,
-		nomefeira,
-		registrot,
-		logradourot,
-		numerot,
-		bairrot,
-		referenciat,
-		createdat,
-		updatedat,
-		deletedat
-	FROM feira where distrito LIKE ? AND 
-	deletedat IS NULL
-	ORDER BY createdat DESC;
+		ID,
+		ID_FEIRA,
+		XLONG,
+		LAT,
+		SETCENS,
+		AREAP,
+		CODDIST,
+		DISTRITO,
+		CODSUBPREF,
+		SUBPREFE,
+		REGIAO5,
+		REGIAO8,
+		NOME_FEIRA,
+		REGISTRO,
+		LOGRADOURO,
+		NUMERO,
+		BAIRRO,
+		REFERENCIA,
+		CREATED_AT,
+		UPDATED_AT,
+		DELETED_AT
+	FROM feira where DISTRITO LIKE ? AND 
+	DELETED_AT IS NULL
+	ORDER BY CREATED_AT DESC;
 	`, "%"+distrito+"%")
 
 	if err != nil {
-		return nil, err
+		logger.ErrorContext(ctx, "store.feira.Search", err.Error())
+		logger.L.Println(time.Now(), "store.feira.Search:", err.Error())
+		return nil, fverr.New(http.StatusNotFound, "não encontrei feira com esses parâmetros", map[string]interface{}{
+			"search": distrito,
+		})
 	}
 
 	return feiras, nil
